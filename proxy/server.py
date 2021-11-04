2cd1ceafa9a6a2d5993b230efab21c471eaf5be1
Chris Latham <backwardspy@gmail.com>
2021-11-04 17:39:32 +0000

    add alternative local wordlist

--- proxy/server.py
+++ proxy/server.py
@@ -1,15 +1,17 @@
 """
-Proxies requests to http://watchout4snakes.com/Random/RandomPhrase
+API that renders phrases based on several wordlist sources.
 """
 
-from dataclasses import dataclass, asdict
+import random
+from contextlib import contextmanager
+from dataclasses import asdict, dataclass
 from enum import Enum, IntEnum
+from typing import Iterator, Protocol, TextIO
+from pathlib import Path
+
+import aiohttp
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import aiohttp
-
-
-API_URL = "http://watchout4snakes.com/Random/RandomPhrase"
 
 
 class PartOfSpeech(Enum):
@@ -67,30 +69,116 @@ class Phrase:
         return self.template.format(*words)
 
 
-async def get_words(parts: list[PhrasePart]) -> list[str]:
+class WordSource(Protocol):
     """
-    Gets words from the API for the given phrase parts.
+    A source of words for the API.
     """
-    form_data = {}
-    for i, part in enumerate(parts):
-        form_data.update(
-            {f"Pos{i+1}": part.pos.value, f"Level{i+1}": int(part.obscurity)}
-        )
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(API_URL, data=form_data) as response:
-            body = await response.text()
-            return body.split()
+    async def get_words(self, parts: list[PhrasePart]) -> list[str]:
+        """
+        Returns a list of words for the given parts of speech.
+        """
+        raise NotImplementedError
 
 
-async def render_phrase(phrase: Phrase) -> tuple[str, list[str]]:
+class Watchout4SnakesSource(WordSource):
+    """
+    Sources words from watchout4snakes.com
+    """
+    URL = "http://watchout4snakes.com/Random/RandomPhrase"
+
+    async def get_words(self, parts: list[PhrasePart]) -> list[str]:
+        """
+        Gets words from the API for the given phrase parts.
+        """
+        form_data = {}
+        for i, part in enumerate(parts):
+            form_data.update(
+                {f"Pos{i+1}": part.pos.value, f"Level{i+1}": int(part.obscurity)}
+            )
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.URL, data=form_data) as response:
+                body = await response.text()
+                return body.split()
+
+
+class VerachellSource(WordSource):
+    """
+    Sources words from the verachell-wordlists submodule.
+    Ignores obscurity as we have no data for it.
+    """
+    BASE_PATH = Path("wordlists/verachell")
+    FILES = {
+        PartOfSpeech.NOUN: [
+            BASE_PATH / "nouns" / "mostly-nouns-ment.txt",
+            BASE_PATH / "nouns" / "mostly-nouns.txt",
+            BASE_PATH / "nouns" / "mostly-plural-nouns.txt",
+        ],
+        PartOfSpeech.ADJECTIVE: [
+            BASE_PATH / "other-categories" / "mostly-adjectives.txt",
+        ],
+        PartOfSpeech.VERB_TRANSITIVE: [
+            BASE_PATH / "verbs" / "transitive-past-tense.txt",
+            BASE_PATH / "verbs" / "transitive-present-tense.txt",
+        ],
+        PartOfSpeech.VERB_INTRANSITIVE: [
+            BASE_PATH / "verbs" / "mostly-verbs-infinitive.txt",
+            BASE_PATH / "verbs" / "mostly-verbs-past-tense.txt",
+            BASE_PATH / "verbs" / "mostly-verbs-present-tense.txt",
+        ],
+        PartOfSpeech.ADVERB: [
+            BASE_PATH / "other-categories" / "ly-adverbs.txt",
+            BASE_PATH / "other-categories" / "mostly-adverbs.txt",
+        ],
+        PartOfSpeech.INTERJECTION: [
+            BASE_PATH / "other-categories" / "mostly-interjections.txt",
+        ],
+        PartOfSpeech.PREPOSITION: [
+            BASE_PATH / "other-categories" / "mostly-prepositions.txt",
+        ],
+    }
+
+    def random_line(self, file: TextIO) -> str:
+        """
+        https://stackoverflow.com/a/3540315
+        """
+        chosen_line = next(file)
+        for num, line in enumerate(file, 2):
+            if random.randrange(num) != 0:
+                continue
+            chosen_line = line
+        return chosen_line
+
+    @contextmanager
+    def random_file(self, pos: PartOfSpeech) -> Iterator[TextIO]:
+        """
+        Returns a random file from the given part of speech.
+        """
+        with random.choice(self.FILES[pos]).open("r") as file:
+            yield file
+
+    def get_word(self, part: PhrasePart) -> str:
+        """
+        Returns a word for the given phrase part.
+        """
+        with self.random_file(part.pos) as file:
+            word = self.random_line(file).strip()
+            if part.title:
+                word = word.title()
+            return word
+
+    async def get_words(self, parts: list[PhrasePart]) -> list[str]:
+        return [self.get_word(part) for part in parts]
+
+
+async def render_phrase(phrase: Phrase, *, source: WordSource) -> tuple[str, list[str]]:
     """
     Renders the given phrase with words from the API.
     """
-    words = await get_words(phrase.parts)
+    words = await source.get_words(phrase.parts)
     cased_words = [
-        word.title() if part.title else word
-        for part, word in zip(phrase.parts, words)
+        word.title() if part.title else word for part, word in zip(phrase.parts, words)
     ]
     return phrase.render(cased_words), words
 
@@ -141,18 +229,25 @@ BBEG = Phrase(
 
 
 app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=[
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "https://randnd.pigeon.life:5500",
-], allow_methods="*", allow_headers="*")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://randnd.pigeon.life:5500",
+    ],
+    allow_methods="*",
+    allow_headers="*",
+)
+
+word_source = VerachellSource()
 
 
 async def make_response(phrase: Phrase) -> dict:
     """
     Makes a response for the given phrase.
     """
-    result, words = await render_phrase(phrase)
+    result, words = await render_phrase(phrase, source=word_source)
     return {"phrase": result, "words": words, "config": asdict(phrase)}
 
 
